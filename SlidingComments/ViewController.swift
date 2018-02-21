86b9756b03ac1a59f400bf74a61325b386d0f8c1
Aline Borges <alinekborges@gmail.com>
2018-02-21 12:02:42 -0300

    Initial Commit

---
+++ SlidingComments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlidingComments
+//
+//  Created by Aline Borges on 21/02/18.
+//  Copyright © 2018 Aline Borges. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
